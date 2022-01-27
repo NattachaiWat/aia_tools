7555a6356e643e0844e2b12574c1fdc135f9384d
Nattachai Watcharapinchai <nuttachai.ton@gmail.com>
2022-01-27 20:22:09 +0700

    add script

---
+++ run_download_analysis.sh
@@ -0,0 +1,2 @@
+export CONNECTION_STRING=
+python download.py --save_path . --url project_code=AIA008 url=https://stbifrostdatadev001.blob.core.windows.net/stbifrostdatadev001/research/AIA008/log/2022-01-27_184837.txt

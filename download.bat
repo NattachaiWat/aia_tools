89a49ad5f08d9a90e2e18f59e98cc18a7659f77e
Nattachai Watcharapinchai <nuttachai.ton@gmail.com>
2022-02-09 12:27:15 +0700

    add download.bat

---
+++ download.bat
@@ -0,0 +1,2 @@
+export CONNECTION_STRING=
+python download_analysis.py --save_path . --url https://stbifrostdatadev001.blob.core.windows.net/stbifrostdatadev001/research/AIA008/log/2022-01-27_184837.txt

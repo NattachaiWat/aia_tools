cad616a73e5acd5588b86ca2272217bc9de5a34e
Nattachai Watcharapinchai <nuttachai.ton@gmail.com>
2022-02-09 12:30:04 +0700

    edit export

--- run_download.bat
+++ run_download.bat
@@ -1,3 +1,4 @@
-export CONNECTION_STRING=
+@echo off
+SET CONNECTION_STRING=
 python download.py --save_path . --url https://stbifrostdatadev001.blob.core.windows.net/stbifrostdatadev001/research/AIA002/excel/2021-12-08_145547_before_train.xlsx
 
